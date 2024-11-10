68e85ceaec65662456603462fbfd191937f4740b
Gilbert00 <fkemper@mail.ru>
2024-11-10 17:07:07 +0300

    Change get_resource for case "json" Adding get_file_ext,get_resource_0,get_resource_1,check_rsrc_json,check_side_json

--- buildconstrains/src/main/java/buildconstrains/CheckResource.java
+++ buildconstrains/src/main/java/buildconstrains/CheckResource.java
@@ -7,6 +7,7 @@ package buildconstrains;
  //import java.time.LocalDate;
 //import java.time.LocalTime;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Date;
@@ -34,6 +35,8 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 //import java.math.BigInteger;
+import com.google.gson.Gson;
+import java.nio.charset.StandardCharsets;
         
 //-------------------
 //-------------------
@@ -42,6 +45,8 @@ public class CheckResource {
 	static ArrayList<String> rsrcY;
 	static int maxlenX;
 	static int maxlenY;
+	static short rsrc_type = -1;
+	static GraphJSON gJSON;
 	
 	static TransportNetDB open_db(String dbFile) {
 		DbHandler.set_conn_base(dbFile);
@@ -60,9 +65,36 @@ public class CheckResource {
 	}
 //--------
 // CheckResource	
+
+    private static boolean get_resource(String fileName) throws IOException{
+		String ext = get_file_ext(fileName);
+		//System.out.printf("ext: %s%n",ext); //TST
+		switch (ext) {
+			case "csv" -> {
+                rsrc_type = 0;
+                return get_resource_0(fileName);
+            }
+			case "json" -> {
+                rsrc_type = 1;
+                return get_resource_1(fileName);
+            }
+			default -> {
+                return false;
+            }
+		}
+	}
+
+	private static String get_file_ext(String fileName){
+		File file = new File(fileName);
+        String fN = file.getName();
+        int dotIndex = fN.lastIndexOf('.');
+        String extension = (dotIndex == -1) ? "" : fileName.substring(dotIndex+1);
+        return extension;
+	}
+
     private static final String COMMA_DELIMITER = ",";
 
-    private static boolean get_resource(String fileName){
+    private static boolean get_resource_0(String fileName){
         try {
 			String rowX, rowY;
 			BufferedReader br = new BufferedReader(new FileReader(fileName));
@@ -93,6 +125,15 @@ public class CheckResource {
         
         return true;
     }
+
+	private static boolean get_resource_1(String fileName) throws IOException{
+		Gson gson = new Gson();
+		String result = Files.readString(Paths.get(fileName), StandardCharsets.UTF_8);
+		gJSON = gson.fromJson(result, GraphJSON.class);
+		//System.out.println(gFrom);			
+		
+		return true;
+	}
 
 	private static ArrayList<String> set_rsrc(String row) {
 		String[] val = row.split(COMMA_DELIMITER);
@@ -127,11 +168,55 @@ public class CheckResource {
 		db.out_calc_result(graph,modeReal);
 	}
 	
+	private static boolean check_rsrc_json(Graph graph, int modeReal) {
+		rsrcX = new ArrayList<>();	
+		rsrcY = new ArrayList<>();
+		
+		if(modeReal==1)
+			return check_side_json(gJSON.X,"X",graph.lX,rsrcX) & 
+				check_side_json(gJSON.Y,"Y",graph.lY,rsrcY);
+		else if(modeReal==2)
+			return check_side_json(gJSON.Y,"Y",graph.lX,rsrcY) & 
+				check_side_json(gJSON.X,"X",graph.lY,rsrcX);			
+		else {
+			System.out.printf("Invalid mode: %d !%n",modeReal);
+			return false;
+		}
+	}
+	
+	private static boolean check_side_json(SideJSON sd, String sdName, ArrayList<String> gSide, ArrayList<String> rsrc) {
+		int ng,nj,i,j;
+		String gVertex, jsonVertex;
+		ng = gSide.size();
+		nj = sd.get_node().size();
+		//rsrcX = new ArrayList<>();
+		int notFound = 0;
+		for(i=0; i<ng; i++) {
+			gVertex = gSide.get(i);
+            //System.out.printf("gVertex:|%s|%n",gVertex);//TST
+			for(j=0; j<nj; j++) {
+				jsonVertex = sd.get_node().get(j);
+				//System.out.printf("jsonVertex:|%s|%n",jsonVertex);//TST
+				if(gVertex.equals(jsonVertex)) {
+					rsrc.add(""+ sd.get_value().get(j));
+					break;
+				}
+			}
+			if(j==nj){
+				notFound++;
+				System.out.printf("Vertex %s don't found in %s resource!%n",gVertex,sdName);
+			}
+		}
+        //System.out.printf("notFound:%d%n",notFound);//TST
+		return (notFound==0);		
+	}
 //--------
 // CheckResource
     static void calc_resource(TransportNetDB db, Limits listR, Graph graph, int modeReal) {
 	//static void calc_resource(TransportNetDB db, Limits listR, String fileResource) throws SQLException {
 		//if(!get_resource(fileResource)) return;
+		if(rsrc_type==1 & !check_rsrc_json(graph, modeReal)) return;
+		
         try {
             if(!check_resource_len(db,rsrcX.size(),rsrcY.size())) return;
             calc_limits(db,modeReal);
@@ -150,7 +235,7 @@ public class CheckResource {
 	}
 //--------
 // CheckResource
- private static void main1(String[] argv) throws SQLException{
+ private static void main1(String[] argv) throws SQLException, IOException{
 
     String fileGraph = argv[0];
     System.out.printf("%s%n",fileGraph);
@@ -238,7 +323,7 @@ public class CheckResource {
 	//return;
   }
  
-  public static void main(String[] argv) throws SQLException {
+  public static void main(String[] argv) throws SQLException, IOException {
  
 	print_current_date();
 	
